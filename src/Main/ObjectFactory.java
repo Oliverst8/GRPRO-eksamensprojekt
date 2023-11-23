b7204d8c8972f85fc7f5ad8e6120cee5a6e75922
0xjogge <61437971+0xjogge@users.noreply.github.com>
2023-11-23 20:24:02 +0100

    ObjectFactory exceptions is now handled directly

--- src/Main/ObjectFactory.java
+++ src/Main/ObjectFactory.java
@@ -9,7 +9,7 @@ import itumulator.world.Location;
 import itumulator.world.World;
 
 public class ObjectFactory {
-    public static Object generate(World world, String className, Object... constructorArgs) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public static Object generate(World world, String className, Object... constructorArgs) {
 
         Object object = generateHelper(className, constructorArgs);
 
@@ -20,7 +20,7 @@ public class ObjectFactory {
         return object;
     }
 
-    public static Object generate(World world, Location location, String className, Object... constructorArgs) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public static Object generate(World world, Location location, String className, Object... constructorArgs) {
 
         Object object = generateHelper(className, constructorArgs);
 
@@ -31,24 +31,52 @@ public class ObjectFactory {
         return object;
     }
 
-    private static Object generateHelper(String className, Object... constructorArgs) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    private static Object generateHelper(String className, Object... constructorArgs) {
         className = className.toLowerCase();
         className = className.substring(0, 1).toUpperCase() + className.substring(1);
         className = "Main." + className;
         
-        // Load the class dynamically
-        Class<?> clazz = Class.forName(className);
+        Constructor<?> constructor;
 
-        // Get the constructor with the specified parameter types
-        Class<?>[] parameterTypes = new Class[constructorArgs.length];
-        for (int i = 0; i < constructorArgs.length; i++) {
-            parameterTypes[i] = constructorArgs[i].getClass();
+        try {
+            // Load the class dynamically
+            Class<?> clazz = Class.forName(className);
+
+            // Get the constructor with the specified parameter types
+            Class<?>[] parameterTypes = new Class[constructorArgs.length];
+            for (int i = 0; i < constructorArgs.length; i++) {
+                parameterTypes[i] = constructorArgs[i].getClass();
+            }
+
+            constructor = clazz.getConstructor(parameterTypes);
+
+            // Create an instance using the specified constructor and arguments
+            return constructor.newInstance(constructorArgs);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found: " + className);
+        } catch (NoSuchMethodException e) {
+            System.out.println("No such constructor: " + className);
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+        } catch (IllegalAccessException e) {
+            System.out.println("No access to constructor" + className);
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+        } catch (InvocationTargetException e) {
+            System.out.println("Could not invoke constructor: " + className);
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+        } catch (InstantiationException e) {
+            System.out.println("Could not instantiate class: " + className);
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
         }
 
-        Constructor<?> constructor = clazz.getConstructor(parameterTypes);
-
-        // Create an instance using the specified constructor and arguments
-        return constructor.newInstance(constructorArgs);
+        // If we get here, we didn't find the class in the Main package
+        return null;
     }
 
     private static void place(World world, Object object) {

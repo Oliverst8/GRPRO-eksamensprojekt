96865ebacd5548dca76cd081a8ab16acc8764838
MessiGames30 <coml@itu.dk>
2023-11-27 16:34:43 +0100

    Har lavet en enkelt test til runtimeexceptions i objectfac generateHelper

--- src/test/ObjectFactoryTest.java
+++ src/test/ObjectFactoryTest.java
@@ -1,26 +1,68 @@
 package test;
 
+import Main.Burrow;
+import Main.ObjectFactory;
+import Main.Rabbit;
+import itumulator.executable.Program;
+import itumulator.world.Location;
+import itumulator.world.World;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ObjectFactoryTest {
 
+    Program program;
+    World world;
+
     @BeforeEach
     void setUp() {
+        int size = 20; // størrelsen af vores 'map' (dette er altid kvadratisk)
+        int delay = 1; // forsinkelsen mellem hver skridt af simulationen (i ms)
+        int display_size = 800; // skærm opløsningen (i px)
+        program = new Program(size, display_size, delay); // opret et nyt program
+        world = program.getWorld(); // hiv verdenen ud, som er der hvor vi skal tilføje ting!
     }
 
+
+    @Test
+    void testgenerateWithoutLocation() {
+        Rabbit rabbit = (Rabbit) ObjectFactory.generate(world, "rabbit");
+        assertEquals(50,rabbit.getHunger());
+    }
+
+    @Test
+    void testgenerateWithLocation() {
+        Location location = new Location(1,1);
+        Rabbit rabbit = (Rabbit) ObjectFactory.generate(world, location,"rabbit");
+        assertEquals(new Location(1,1),world.getLocation(rabbit));
+    }
+
+    /**
+     * https://stackoverflow.com/questions/8708342/redirect-console-output-to-string-in-java
+     */
+    @Test
+    void testGenerateWithUnknownClassname(){
+        ByteArrayOutputStream placeholder = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(placeholder);
+        PrintStream old = System.out;
+        System.setOut(printStream);
+        System.setOut(printStream);
+        try {
+            Rabbit rabbit = (Rabbit) ObjectFactory.generate(world,"UkendtKunstner");
+        } catch (Exception e) {
+            assertTrue(placeholder.toString().contains("Class not found: Main.Ukendtkunstner"));
+        }
+
+
+    }
     @AfterEach
     void tearDown() {
     }
 
-    @Test
-    void generate() {
-    }
-
-    @Test
-    void testGenerate() {
-    }
 }

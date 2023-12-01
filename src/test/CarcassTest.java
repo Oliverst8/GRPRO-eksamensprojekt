c1dd9a09a1584d5a4ed7ab7e8e5908192b9baa7b
oliverst8 <oliver@starup99.dk>
2023-12-01 17:19:19 +0100

    Created  Carcass class

---
+++ src/test/CarcassTest.java
@@ -0,0 +1,60 @@
+package test;
+import Main.Carcass;
+import Main.Entity;
+import Main.Rabbit;
+import itumulator.executable.Program;
+import itumulator.world.Location;
+import itumulator.world.World;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spawn.ObjectFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class CarcassTest {
+
+    Program program;
+    World world;
+
+    @BeforeEach
+    void setUp(){
+        int size = 20; // størrelsen af vores 'map' (dette er altid kvadratisk)
+        int delay = 1; // forsinkelsen mellem hver skridt af simulationen (i ms)
+        int display_size = 800; // skærm opløsningen (i px)
+        program = new Program(size, display_size, delay); // opret et nyt program
+        world = program.getWorld(); // hiv verdenen ud, som er der hvor vi skal tilføje ting!
+    }
+
+    @Test
+    void testIfCarcussSpawnsWhenAnimalDies(){
+        Location location = new Location(0,0);
+        Rabbit rabbit = (Rabbit) ObjectFactory.generateOnMap(world, location, "rabbit");
+        rabbit.setEnergy(0);
+        program.simulate();
+        assertTrue(world.getTile(location).getClass().equals(Carcass.class));
+    }
+
+    @Test
+    void testGetEntityClassOverride() {
+        Location location = new Location(0,0);
+        Rabbit rabbit = (Rabbit) ObjectFactory.generateOnMap(world, location, "rabbit");
+        rabbit.setEnergy(0);
+        program.simulate();
+        assertTrue(((Entity) world.getTile(location)).getEntityClass().equals(rabbit.getEntityClass()));
+    }
+
+    @Test
+    void testCarcassDissapersAfter3Nights() {
+        Location location = new Location(0,0);
+        Rabbit rabbit = (Rabbit) ObjectFactory.generateOnMap(world, location, "rabbit");
+        rabbit.setEnergy(0);
+        int adultAge = 3;
+        for (int i = 0; i < adultAge*19; i++) {
+            program.simulate();
+            assertNotNull(world.getTile(location));
+        }
+        for(int i = 0; i < adultAge; i++){
+            program.simulate();
+        }
+        assertNull(world.getTile(location));
+    }
+}

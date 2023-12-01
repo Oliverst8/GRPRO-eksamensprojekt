c5762a502f229c653e3e91667fac0276091f13c5
oliverst8 <oliver@starup99.dk>
2023-12-01 15:11:35 +0100

    Made findNearestMember method

--- src/Main/Pack.java
+++ src/Main/Pack.java
@@ -1,9 +1,17 @@
 package Main;
 
 import itumulator.world.Location;
+import itumulator.world.World;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Pack extends Community{
-    Den den;
+    private Den den;
+
+    public Pack(){
+        den = null;
+    }
 
     public void createDen(Location denLocation){
         this.den = new Den(denLocation);
@@ -12,4 +20,15 @@ public class Pack extends Community{
     public Location getDenLocation(){
         return den.getLocation();
     }
+
+    public Den getDen() {
+        return den;
+    }
+
+    public Location findNearestMember(World world){
+        List<Animal> wolvesAboveGround = new LinkedList<>(getMembers());
+        if(den != null) wolvesAboveGround.removeAll(den.getMembers());
+        return findNearestEntity(world, wolvesAboveGround);
+    }
+
 }

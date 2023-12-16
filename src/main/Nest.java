62dfbfd944537646c6dadcd8b557500ccc7f582d
MessiGames30 <coml@itu.dk>
2023-12-16 17:01:11 +0100

    java documentation made in nest

--- src/main/Nest.java
+++ src/main/Nest.java
@@ -3,11 +3,27 @@ package main;
 import java.util.Set;
 
 public interface Nest{
+    /**
+     * Adds a member to the nest
+     * @param member the member to add.
+     */
     abstract void addMember(Animal member);
 
+    /**
+     * Removes a member from the next
+     * @param member the member to remove
+     */
     abstract void removeMember(Animal member);
 
+    /**
+     * gets Members from the nest
+     * @return a Set containing all of the members of the nest
+     */
     abstract Set<Animal> getMembers();
 
+    /**
+     * Gets adult members from the nest
+     * @return a Set containing all of the adult members of the nest
+     */
     abstract Set<Animal> getAdultMembers();
 }
